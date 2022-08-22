11acddc87291433426d07cee2e7809be09e0940a
sandeep-katta <sandeep.katta2007@gmail.com>
2022-08-22 19:16:31 +0530

    Added SparkAddUDF

--- src/main/scala/com/databricks/spark/SparkAddUDF.scala
+++ src/main/scala/com/databricks/spark/SparkAddUDF.scala
@@ -3,7 +3,7 @@ package com.databricks.spark
 import org.apache.hadoop.hive.ql.exec.UDF;
 class SparkAddUDF extends UDF {
 
-    def evaluate(a: Int, b: Int): Int = {
+    def add_int(a: Int, b: Int): Int = {
         a + b
     }
 
